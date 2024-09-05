21350e017d48c261e1fe120c996917a095dbd7c9
Vishnu C Prasad <mail@vishnucprasad.in>
2024-09-05 12:07:07 +0530

    Fixed chat duplication issue

--- libs/common/src/filters/ws-exception.filter.ts
+++ libs/common/src/filters/ws-exception.filter.ts
@@ -4,20 +4,29 @@ import {
   ExceptionFilter,
   HttpException,
   BadRequestException,
+  ConflictException,
 } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
 
-@Catch(WsException, HttpException, BadRequestException)
+@Catch(WsException, HttpException, BadRequestException, ConflictException)
 export class WsExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToWs();
     const client = ctx.getClient();
 
+    let code = 'UNEXPECTED_EXCEPTION';
     let errorMessage = 'An error occurred';
 
     if (exception instanceof BadRequestException) {
       const response = exception.getResponse();
       if (typeof response === 'object' && response['message']) {
+        code = 'VALIDATION_EXCEPTION';
+        errorMessage = response['message']; // Extract validation errors
+      }
+    } else if (exception instanceof ConflictException) {
+      const response = exception.getResponse();
+      if (typeof response === 'object' && response['message']) {
+        code = 'CONFLICT_EXCEPTION';
         errorMessage = response['message']; // Extract validation errors
       }
     } else if (
@@ -28,7 +37,7 @@ export class WsExceptionFilter implements ExceptionFilter {
     }
 
     client.emit('EXCEPTION', {
-      status: 'Web socket exception',
+      code: code,
       message: errorMessage,
     });
   }
--- src/chat/schemas/chat.schema.ts
+++ src/chat/schemas/chat.schema.ts
@@ -1,14 +1,15 @@
 import { BaseEntity } from '@conversa/database';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ChatType } from '../types/chat.types';
-import { Types } from 'mongoose';
+import { Model, Types } from 'mongoose';
+import { ConflictException } from '@nestjs/common';
 
 @Schema({ versionKey: false, timestamps: true, collection: 'chats' })
 export class Chat extends BaseEntity {
-  @Prop({ type: String, enum: ChatType, requried: true })
+  @Prop({ type: String, enum: ChatType, required: true })
   type: ChatType;
 
-  @Prop({ required: true, minlength: 2 })
+  @Prop({ type: [Types.ObjectId], required: true })
   members: Array<Types.ObjectId>;
 
   @Prop()
@@ -17,14 +18,34 @@ export class Chat extends BaseEntity {
   @Prop()
   chatIcon?: string;
 
-  @Prop()
+  @Prop({ type: [Types.ObjectId] })
   admins?: Array<Types.ObjectId>;
 
-  @Prop({ default: [] })
+  @Prop({ type: [Types.ObjectId], default: [] })
   messages?: Array<Types.ObjectId>;
 
-  @Prop()
+  @Prop({ type: Types.ObjectId })
   lastMessage?: Types.ObjectId;
 }
 
 export const ChatSchema = SchemaFactory.createForClass(Chat);
+
+ChatSchema.pre('save', async function (next) {
+  if (this.isNew && this.type === ChatType.PRIVATE) {
+    const existingChat = await (this.constructor as Model<Chat & Document>)
+      .findOne({
+        type: ChatType.PRIVATE,
+        members: { $all: this.members },
+      })
+      .exec();
+
+    if (existingChat) {
+      return next(
+        new ConflictException(
+          'A private chat with the same members already exists.',
+        ),
+      );
+    }
+  }
+  next();
+});
